88e14efea2a83a2cca3dcd1b89772339d455857c
mosesmbadi <mosesmbadi@gmail.com>
2022-07-23 15:31:22 +0300

    uploaded to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=mosesmbadi/machine-learning-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u mosesmbadi
+docker tag machine-learning-api $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
